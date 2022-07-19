b8793519e616aee0283f2f93690a438890954b72
George S <george.lmfao@gmail.com>
2022-07-19 01:14:32 +0000

    Organized Elements, Listeners, and Functions into their own objects.

--- script.js
+++ script.js
@@ -6,176 +6,180 @@ class AWDice {
   }
 
   roll() {
-    const result = new Object();
-    result.d1 = Math.floor(Math.random() * this.sides) + 1;
-    result.d2 = Math.floor(Math.random() * this.sides) + 1;
+    const result = {
+      d1: Math.floor(Math.random() * this.sides) + 1,
+      d2: Math.floor(Math.random() * this.sides) + 1
+    }
     return result;
   }
 }
 
-// Dice Roller with Modifiers Elements
-const modifierElements = {
-  negThree: document.querySelector("#rollNegThree"),
-  negTwo: document.querySelector("#rollNegTwo"),
-  negOne: document.querySelector("#rollNegOne"),
-  zero: document.querySelector("#rollZero"),
-  one: document.querySelector("#rollOne"),
-  two: document.querySelector("#rollTwo"),
-  three: document.querySelector("#rollThree"),
-  four: document.querySelector("#rollFour"),
-  success: document.querySelector("#success"),
-  fumble: document.querySelector("#fumble"),
-  fail: document.querySelector("#fail")
+const Elements = {
+  rollWithModifier: {
+    negThree: document.querySelector("#rollNegThree"),
+    negTwo: document.querySelector("#rollNegTwo"),
+    negOne: document.querySelector("#rollNegOne"),
+    zero: document.querySelector("#rollZero"),
+    one: document.querySelector("#rollOne"),
+    two: document.querySelector("#rollTwo"),
+    three: document.querySelector("#rollThree"),
+    four: document.querySelector("#rollFour"),
+    success: document.querySelector("#success"),
+    fumble: document.querySelector("#fumble"),
+    fail: document.querySelector("#fail"),
+    input1: document.getElementById("input1"),
+    input2: document.getElementById("input2"),
+    input3: document.getElementById("input3"),
+    output: document.getElementById("output")
+  },
+  scaleCollapser: {
+    head: document.querySelector("#successScaleHead"),
+    prob: document.querySelector("#successScaleProbabilityHead"),
+    body: document.querySelector("#successScaleBody")
+  },
+  calculateHarm: {
+    submitHarm: document.querySelector("#submitHarm"),
+    weaponHarm: document.querySelector("#weaponHarm"),
+    armorHarm: document.querySelector("#armorHarm"),
+    netHarm: document.querySelector("#netHarm")
+  },
+  basicMoves: {
+    selector: document.querySelector("#basicMovesSelector"),
+    button: document.querySelector("#getMove"),
+    current: null
+  }
 };
 
-// Success Scale Collapser Elements
-const scaleCollapser = {
-  head: document.querySelector("#successScaleHead"),
-  prob: document.querySelector("#successScaleProbabilityHead"),
-  body: document.querySelector("#successScaleBody")
-}
-
-// Harm Calculation Elements
-const submitHarm = document.querySelector("#submitHarm");
-
-// Basic Moves Elements
-const basicMoves = {
-  selector: document.querySelector("#basicMovesSelector"),
-  button: document.querySelector("#getMove"),
-  current: null
+const Listeners = {
+  rollWithModifier: {
+    l1: Elements.rollWithModifier.negThree.addEventListener("click", () => { Functions.statRollWithModifier(-3) }),
+    l2: Elements.rollWithModifier.negTwo.addEventListener("click", () => { Functions.statRollWithModifier(-2) }),
+    l3: Elements.rollWithModifier.negOne.addEventListener("click", () => { Functions.statRollWithModifier(-1) }),
+    l4: Elements.rollWithModifier.zero.addEventListener("click", () => { Functions.statRollWithModifier(0) }),
+    l5: Elements.rollWithModifier.one.addEventListener("click", () => { Functions.statRollWithModifier(1) }),
+    l6: Elements.rollWithModifier.two.addEventListener("click", () => { Functions.statRollWithModifier(2) }),
+    l7: Elements.rollWithModifier.three.addEventListener("click", () => { Functions.statRollWithModifier(3) }),
+    l8: Elements.rollWithModifier.four.addEventListener("click", () => { Functions.statRollWithModifier(4) })
+  },
+  scaleCollapser: {
+    l1: Elements.scaleCollapser.head.addEventListener("click", () => { Functions.collapseSuccessScale() })
+  },
+  calculateHarm: {
+    l1: Elements.calculateHarm.submitHarm.addEventListener("click", (e) => { Functions.calculateHarm(e) })
+  },
+  basicMoves: {
+    l1: Elements.basicMoves.button.addEventListener("click", (e) => { Functions.displayBasicMove(e) })
+  }
 };
 
-// Dice Roller with Modifiers EventListeners
-const diceRoller = {
-  dice1: modifierElements.negThree.addEventListener("click", () => { statRollWithModifier(-3) }),
-  dice2: modifierElements.negTwo.addEventListener("click", () => { statRollWithModifier(-2) }),
-  dice3: modifierElements.negOne.addEventListener("click", () => { statRollWithModifier(-1) }),
-  dice4: modifierElements.zero.addEventListener("click", () => { statRollWithModifier(0) }),
-  dice5: modifierElements.one.addEventListener("click", () => { statRollWithModifier(1) }),
-  dice6: modifierElements.two.addEventListener("click", () => { statRollWithModifier(2) }),
-  dice7: modifierElements.three.addEventListener("click", () => { statRollWithModifier(3) }),
-  dice8: modifierElements.four.addEventListener("click", () => { statRollWithModifier(4) })
-}
+const Functions = {
+  statRollWithModifier: (modifier) => {
 
-// Success Scale Collapser EventListeners
-scaleCollapser.head.addEventListener("click", () => { collapseSuccessScale() })
+    const dice = new AWDice(6);
+    const result = dice.roll();
+    const probability = new Object();
+    //Success Distribution
+    const d = {
+      r2: 2.78,
+      r3: 5.56,
+      r4: 8.33,
+      r5: 11.11,
+      r6: 13.89,
+      r7: 16.67,
+      r8: 13.89,
+      r9: 11.11,
+      r10: 8.33,
+      r11: 5.56,
+      r12: 2.78
+    };
 
-// Harm Calculation EventListeners
-submitHarm.addEventListener("click", (e) => { calculateHarm(e) });
+    if (modifier === -3) {
 
-// Basic Moves EventListeners
-basicMoves.button.addEventListener("click", (e) => { displayBasicMove(e) });
+      probability.success = 0.00;
+      probability.fumble = d.r12 + d.r11 + d.r10 - 0.01;
+      probability.fail = d.r9 + d.r8 + d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
 
-// Dice Roller with Modifiers Functions
-function statRollWithModifier(modifier) {
-  const dice = new AWDice(6);
-  const result = dice.roll();
-  const probability = new Object();
-  // Distribution
-  const d = {
-    r2: 2.78,
-    r3: 5.56,
-    r4: 8.33,
-    r5: 11.11,
-    r6: 13.89,
-    r7: 16.67,
-    r8: 13.89,
-    r9: 11.11,
-    r10: 8.33,
-    r11: 5.56,
-    r12: 2.78
-  };
+    } else if (modifier === -2) {
 
-  if (modifier === -3) {
+      probability.success = d.r12 - 0.01;
+      probability.fumble = d.r11 + d.r10 + d.r9;
+      probability.fail = d.r8 + d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
 
-    probability.success = 0.00;
-    probability.fumble = d.r12 + d.r11 + d.r10 - 0.01;
-    probability.fail = d.r9 + d.r8 + d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
+    } else if (modifier === -1) {
 
-  } else if (modifier === -2) {
+      probability.success = d.r12 + d.r11 - 0.01;
+      probability.fumble = d.r10 + d.r9 + d.r8;
+      probability.fail = d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
 
-    probability.success = d.r12 - 0.01;
-    probability.fumble = d.r11 + d.r10 + d.r9;
-    probability.fail = d.r8 + d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
+    } else if (modifier === 0) {
 
-  } else if (modifier === -1) {
+      probability.success = d.r12 + d.r11 + d.r10 - 0.01;
+      probability.fumble = d.r9 + d.r8 + d.r7;
+      probability.fail = d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
 
-    probability.success = d.r12 + d.r11 - 0.01;
-    probability.fumble = d.r10 + d.r9 + d.r8;
-    probability.fail = d.r7 + d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
+    } else if (modifier === 1) {
 
-  } else if (modifier === 0) {
+      probability.success = d.r12 + d.r11 + d.r10 + d.r9 - 0.01;
+      probability.fumble = d.r8 + d.r7 + d.r6;
+      probability.fail = d.r5 + d.r4 + d.r3 + d.r2;
 
-    probability.success = d.r12 + d.r11 + d.r10 - 0.01;
-    probability.fumble = d.r9 + d.r8 + d.r7;
-    probability.fail = d.r6 + d.r5 + d.r4 + d.r3 + d.r2;
+    } else if (modifier === 2) {
 
-  } else if (modifier === 1) {
+      probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 - 0.01;
+      probability.fumble = d.r7 + d.r6 + d.r5;
+      probability.fail = d.r4 + d.r3 + d.r2;
 
-    probability.success = d.r12 + d.r11 + d.r10 + d.r9 - 0.01;
-    probability.fumble = d.r8 + d.r7 + d.r6;
-    probability.fail = d.r5 + d.r4 + d.r3 + d.r2;
+    } else if (modifier === 3) {
 
-  } else if (modifier === 2) {
+      probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 + d.r7 - 0.01;
+      probability.fumble = d.r6 + d.r5 + d.r4;
+      probability.fail = d.r3 + d.r2;
 
-    probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 - 0.01;
-    probability.fumble = d.r7 + d.r6 + d.r5;
-    probability.fail = d.r4 + d.r3 + d.r2;
+    } else if (modifier === 4) {
 
-  } else if (modifier === 3) {
+      probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 + d.r7 + d.r6 - 0.01;
+      probability.fumble = d.r5 + d.r4 + d.r3;
+      probability.fail = d.r2;
 
-    probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 + d.r7 - 0.01;
-    probability.fumble = d.r6 + d.r5 + d.r4;
-    probability.fail = d.r3 + d.r2;
+    } else {
+      alert(`Invalid Input in Functions.rollWithModifier()`);
+    }
 
-  } else if (modifier === 4) {
+    Elements.rollWithModifier.input1.textContent = result.d1;
+    Elements.rollWithModifier.input2.textContent = result.d2;
+    Elements.rollWithModifier.input3.textContent = modifier;
+    Elements.rollWithModifier.output.textContent = result.d1 + result.d2 + modifier;
+    Elements.rollWithModifier.success.textContent = probability.success.toFixed(2);
+    Elements.rollWithModifier.fumble.textContent = probability.fumble.toFixed(2);
+    Elements.rollWithModifier.fail.textContent = probability.fail.toFixed(2);
+  },
+  collapseSuccessScale: () => {
+    Elements.scaleCollapser.body.classList.toggle("hidden");
+    Elements.scaleCollapser.prob.classList.toggle("hidden");
+  },
+  calculateHarm: (e) => {
+    e.preventDefault();
 
-    probability.success = d.r12 + d.r11 + d.r10 + d.r9 + d.r8 + d.r7 + d.r6 - 0.01;
-    probability.fumble = d.r5 + d.r4 + d.r3;
-    probability.fail = d.r2;
+    let result = Elements.calculateHarm.weaponHarm.value - Elements.calculateHarm.armorHarm.value;
+    result = (result < 0) ? 0 : result;
+
+    Elements.calculateHarm.netHarm.textContent = result;
+  },
+  displayBasicMove: (e) => {
+    e.preventDefault();
+
+    if (Elements.basicMoves.current != null) {
+      Elements.basicMoves.current.classList.toggle("hidden");
+      Elements.basicMoves.current = document.querySelector(`#${Elements.basicMoves.selector.value}`);
+      Elements.basicMoves.current.classList.toggle("hidden");
+
+    } else {
+
+      Elements.basicMoves.current = document.querySelector(`#${Elements.basicMoves.selector.value}`);
+      Elements.basicMoves.current.classList.toggle("hidden");
+
+    }
 
   }
+};
 
-  document.getElementById("input1").textContent = result.d1;
-  document.getElementById("input2").textContent = result.d2;
-  document.getElementById("input3").textContent = modifier;
-  document.getElementById("output").textContent = result.d1 + result.d2 + modifier;
-  modifierElements.success.textContent = probability.success.toFixed(2);
-  modifierElements.fumble.textContent = probability.fumble.toFixed(2);
-  modifierElements.fail.textContent = probability.fail.toFixed(2);
-
-}
-
-// Success Scale Collapser functions
-function collapseSuccessScale() {
-  scaleCollapser.body.classList.toggle("hidden");
-  scaleCollapser.prob.classList.toggle("hidden");
-}
-
-// Harm Calculation Functions
-function calculateHarm(e) {
-  e.preventDefault();
-  
-  const weaponHarm = document.querySelector("#weaponHarm").value;
-  const armorHarm = document.querySelector("#armorHarm").value;
-  let result = weaponHarm - armorHarm;
-
-  result = (result < 0) ? 0 : result;
-
-  document.querySelector("#netHarm").textContent = result;
-}
-
-// Basic Moves EventListeners
-function displayBasicMove(e) {
-  e.preventDefault();
-
-  if (basicMoves.current != null) {
-    basicMoves.current.classList.toggle("hidden");
-    basicMoves.current = document.querySelector(`#${basicMoves.selector.value}`);
-    basicMoves.current.classList.toggle("hidden");
-  } else {
-    basicMoves.current = document.querySelector(`#${basicMoves.selector.value}`);
-    basicMoves.current.classList.toggle("hidden");
-  }
-
-}
